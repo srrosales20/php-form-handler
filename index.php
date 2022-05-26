6c2b6b72d3c93dec682ce61d1fa896c10016f786
Sonia Rosales <103076929+srrosales20@users.noreply.github.com>
2022-05-26 16:43:15 +0000

    worlds smallest handler

--- index.php
+++ index.php
@@ -1,15 +1,27 @@
 <html>
   <head>
-    <title>PHP Test Page</title>
+    <title>Worlds Smallest Handler</title>
   </head>
   <body>
-    <h1>PHP Test Page</h1>
-    <?php
-    echo '<p>This is PHP!</p>';
-    ?>
-    <p>Hello from Github!</p>
+    <h1>Worlds Smallest Handler</h1>
     <?php 
-echo '<p>This is a change from repl.it!</p>'; ?>
+  if(isset($_POST["FirstName"])){//show data
+    echo $_POST["FirstName"];
+  }else{//show form
+    echo '
+          <form method="post" action="">
+          <p>First Name: <input type="text" name="FirstName"/>           </p>
+          <p><input type="submit" /></p>
+          </form>
+
+         ';
+  }
+
+
+
+
+    ?>
+
   
   </body>
 </html>
